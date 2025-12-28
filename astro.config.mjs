83ba0a6881b9de460b2f639590310c9685644783
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2025-12-28 13:03:01 +0200

    Add greeting box as island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-     site: "https://mika-astro-test.netlify.app/"
+  site: "https://mika-astro-test.netlify.app/",
+  integrations: [preact()]
 });
